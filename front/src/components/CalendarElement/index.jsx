7a093ad99cc42f974ea2f7eecb3fefc52f76d079
ryota kise <ryota1839@gmail.com>
2020-08-07 21:49:29 +0900

    3-4

--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import * as styles from "./style.css";
 import { Typography } from "@material-ui/core";
 import dayjs from "dayjs";
-import { isSameMonth, isFirstDay, isSameDay } from "../../services/calendar";
+import {
+  isSameDay,
+  isSameMonth,
+  isFirstDay,
+  getMonth
+} from "../../services/calendar";
 
-const CalendarElement = ({ day }) => {
-  const format = isFirstDay(day) ? "M月M日" : "D";
+const CalendarElement = ({ day, month }) => {
+  const format = isFirstDay(day) ? "M月D日" : "D";
   const today = dayjs();
   const isToday = isSameDay(day, today);
-  const isCurrentMonth = isSameMonth(day, today);
+  const currentMonth = getMonth(month);
+  const isCurrentMonth = isSameMonth(day, currentMonth);
   const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
   return (
     <div className={styles.element}>
