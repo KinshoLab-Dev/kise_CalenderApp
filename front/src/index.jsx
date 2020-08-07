37b5841ed9b8a1490de94ae6b4fa79ff548e1d48
ryota kise <ryota1839@gmail.com>
2020-08-07 20:11:55 +0900

    fix: 3-3

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -6,6 +6,7 @@ import "dayjs/locale/ja";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./redux/rootReducer";
+import Navigation from "./components/Navigation/container"
 
 const store = createStore(rootReducer);
 
@@ -13,6 +14,7 @@ dayjs.locale("ja");
 
 const App = () => (
   <Provider store={store}>
+    <Navigation/>
     <CalendarBoard />
   </Provider>
 )
